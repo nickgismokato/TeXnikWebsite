bc59a3a68f9ecf4833fe109cbaa17dc80bbb376a
Nickgismokato <nickvillumlaursen@gmail.com>
2025-02-10 05:02:16 +0100

    Adding Satyr ico and .jpg. Testing image path.

--- src/main/java/com/nickgismokato/TeXnik/ui/View/WishListView.java
+++ src/main/java/com/nickgismokato/TeXnik/ui/View/WishListView.java
@@ -2,9 +2,11 @@ package com.nickgismokato.TeXnik.ui.View;
 
 import com.nickgismokato.TeXnik.ui.MainView;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
 
 @Route(value = "Wishlist", layout = MainView.class)
 @PageTitle("Wishlist")
@@ -14,5 +16,7 @@ public class WishListView extends VerticalLayout{
 	public WishListView(){
 		pageName = new Text("WishListView");
 		add(pageName);
+		Image image = new Image("images/Satyr.jpg", "Satyr Image");
+		add(image);
 	}
 }
